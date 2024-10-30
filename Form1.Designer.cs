90f9e630b7b1d266a03a1a52e95553efc23736b5
somonor011 <somonorhong011@gmail.com>
2024-10-30 20:17:07 +0700

    Interface1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(25, 44);
+            label1.Location = new Point(37, 37);
             label1.Name = "label1";
             label1.Size = new Size(66, 20);
             label1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(25, 93);
+            label2.Location = new Point(37, 86);
             label2.Name = "label2";
             label2.Size = new Size(124, 20);
             label2.TabIndex = 1;
@@ -64,7 +64,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(25, 142);
+            label3.Location = new Point(37, 135);
             label3.Name = "label3";
             label3.Size = new Size(128, 20);
             label3.TabIndex = 2;
@@ -74,7 +74,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(25, 195);
+            label4.Location = new Point(37, 188);
             label4.Name = "label4";
             label4.Size = new Size(42, 20);
             label4.TabIndex = 3;
@@ -83,16 +83,17 @@
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(157, 44);
+            textBox1.Location = new Point(169, 37);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(168, 27);
             textBox1.TabIndex = 4;
             // 
             // label5
             // 
+            label5.BackColor = Color.FromArgb(224, 224, 224);
             label5.BorderStyle = BorderStyle.Fixed3D;
             label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(157, 93);
+            label5.Location = new Point(169, 86);
             label5.Name = "label5";
             label5.Size = new Size(168, 25);
             label5.TabIndex = 5;
@@ -101,7 +102,7 @@
             // 
             label6.BorderStyle = BorderStyle.Fixed3D;
             label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(157, 142);
+            label6.Location = new Point(169, 135);
             label6.Name = "label6";
             label6.Size = new Size(168, 25);
             label6.TabIndex = 6;
@@ -110,7 +111,7 @@
             // 
             label7.BorderStyle = BorderStyle.Fixed3D;
             label7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(157, 190);
+            label7.Location = new Point(169, 183);
             label7.Name = "label7";
             label7.Size = new Size(168, 25);
             label7.TabIndex = 7;
@@ -118,7 +119,7 @@
             // button1
             // 
             button1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(55, 249);
+            button1.Location = new Point(83, 236);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 8;
@@ -128,7 +129,7 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(197, 249);
+            button2.Location = new Point(200, 236);
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 9;
@@ -139,6 +140,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
             ClientSize = new Size(380, 290);
             Controls.Add(button2);
             Controls.Add(button1);
