0f138aa7133dfe067b6eb8418f6e0ab4a3edf07f
somonor011 <somonorhong011@gmail.com>
2024-11-01 00:35:47 +0700

    design color

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,56 +34,57 @@
             label4 = new Label();
             textBox1 = new TextBox();
             label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
             button1 = new Button();
             button2 = new Button();
+            label6 = new Label();
+            label7 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(37, 37);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(30, 38);
             label1.Name = "label1";
-            label1.Size = new Size(66, 20);
+            label1.Size = new Size(68, 20);
             label1.TabIndex = 0;
             label1.Text = "Subtotal";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(37, 86);
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(30, 87);
             label2.Name = "label2";
-            label2.Size = new Size(124, 20);
+            label2.Size = new Size(128, 20);
             label2.TabIndex = 1;
             label2.Text = "Discount Percent";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(37, 135);
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(30, 136);
             label3.Name = "label3";
-            label3.Size = new Size(128, 20);
+            label3.Size = new Size(133, 20);
             label3.TabIndex = 2;
             label3.Text = "Discount Amount";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(37, 188);
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(30, 189);
             label4.Name = "label4";
-            label4.Size = new Size(42, 20);
+            label4.Size = new Size(44, 20);
             label4.TabIndex = 3;
             label4.Text = "Total";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(169, 37);
+            textBox1.Location = new Point(200, 37);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(168, 27);
             textBox1.TabIndex = 4;
@@ -93,61 +94,68 @@
             label5.BackColor = Color.FromArgb(224, 224, 224);
             label5.BorderStyle = BorderStyle.Fixed3D;
             label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(169, 86);
+            label5.Location = new Point(200, 86);
             label5.Name = "label5";
             label5.Size = new Size(168, 25);
             label5.TabIndex = 5;
-            // 
-            // label6
-            // 
-            label6.BorderStyle = BorderStyle.Fixed3D;
-            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(169, 135);
-            label6.Name = "label6";
-            label6.Size = new Size(168, 25);
-            label6.TabIndex = 6;
-            // 
-            // label7
-            // 
-            label7.BorderStyle = BorderStyle.Fixed3D;
-            label7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(169, 183);
-            label7.Name = "label7";
-            label7.Size = new Size(168, 25);
-            label7.TabIndex = 7;
+            label5.Click += label5_Click;
             // 
             // button1
             // 
-            button1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(83, 236);
+            button1.BackColor = Color.SeaGreen;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(83, 239);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 8;
             button1.Text = "Calculate";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(200, 236);
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(200, 239);
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 3;
             button2.Text = "Exit";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.FromArgb(224, 224, 224);
+            label6.BorderStyle = BorderStyle.Fixed3D;
+            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(200, 136);
+            label6.Name = "label6";
+            label6.Size = new Size(168, 25);
+            label6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.BackColor = Color.FromArgb(224, 224, 224);
+            label7.BorderStyle = BorderStyle.Fixed3D;
+            label7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(200, 188);
+            label7.Name = "label7";
+            label7.Size = new Size(168, 25);
+            label7.TabIndex = 10;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(224, 224, 224);
-            ClientSize = new Size(380, 290);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            BackColor = Color.MediumTurquoise;
+            ClientSize = new Size(410, 290);
             Controls.Add(label7);
             Controls.Add(label6);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(label5);
             Controls.Add(textBox1);
             Controls.Add(label4);
@@ -168,9 +176,9 @@
         private Label label4;
         private TextBox textBox1;
         private Label label5;
-        private Label label6;
-        private Label label7;
         private Button button1;
         private Button button2;
+        private Label label6;
+        private Label label7;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace InvoiceTotalForm
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
